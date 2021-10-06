3ff33dcd04fefb2e9841df59a3a3d7e57a33b2d1
João Henrique <joaoenrique.san@gmail.com>
2021-10-06 14:08:17 -0300

    Finish base layout of mainPage

--- src/mainPage.js
+++ src/mainPage.js
@@ -1,14 +1,41 @@
 import './styles.css';
+import FoodImage from './img/food2.jpg';
 
 const createMainPage = () =>{
     let main = document.createElement('main');
+    let background = document.createElement('div');
     let title = document.createElement('h1');
-
+    
+    // TITLE AND BACKGROUND
     title.innerHTML = "REST AU RANT";
     title.classList.add('title-text');
     
-    main.classList.add('background');
-    main.appendChild(title);
+    background.classList.add('background');
+    background.appendChild(title);
+    // -------------------------------
+
+    let contentContainer = document.createElement('div');
+    let textContainerElement = document.createElement('div');
+
+    let sectionTitle = document.createElement('h2');
+    sectionTitle.innerHTML = "LOREM AU RANT";
+
+    let sectionText = document.createElement('p');
+    sectionText.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet malesuada sapien. Etiam leo neque, condimentum sed sollicitudin vitae, ultricies id tortor. Aliquam pellentesque at turpis at mollis. Donec a ipsum odio. Etiam faucibus tortor a laoreet laoreet. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer lobortis nulla id sapien ullamcorper sagittis. Proin in'
+    
+    textContainerElement.appendChild(sectionTitle);
+    textContainerElement.appendChild(sectionText);
+    let foodImgElement = new Image();
+
+    foodImgElement.src = FoodImage;
+
+    contentContainer.appendChild(foodImgElement);
+
+    contentContainer.appendChild(textContainerElement);
+    contentContainer.classList.add('content-container');
+
+    main.appendChild(background);
+    main.appendChild(contentContainer);
 
     return main;
 }
