98f3f0570c7525784e686052afa4cb79ecb91fc3
João Henrique Santana <joaoenrique.san@gmail.com>
2021-10-06 11:39:57 -0300

    Add styles.css and configure webpack

--- src/mainPage.js
+++ src/mainPage.js
@@ -1,7 +1,11 @@
+import './styles.css';
+
 const createMainPage = () =>{
     let main = document.createElement('main');
     let title = document.createElement('h1');
     title.innerHTML = "REST AU RANT";
+
+    main.classList.add('background');
     main.appendChild(title);
 
     return main;
