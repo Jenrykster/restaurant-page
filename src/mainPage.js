7e6a08fd9f313156254da4f32abb51ab477b1915
João Henrique Santana <joaoenrique.san@gmail.com>
2021-10-06 12:08:32 -0300

    Style background of mainPage

--- src/mainPage.js
+++ src/mainPage.js
@@ -3,8 +3,10 @@ import './styles.css';
 const createMainPage = () =>{
     let main = document.createElement('main');
     let title = document.createElement('h1');
-    title.innerHTML = "REST AU RANT";
 
+    title.innerHTML = "REST AU RANT";
+    title.classList.add('title-text');
+    
     main.classList.add('background');
     main.appendChild(title);
 
